9f9998c55c6b0fbcbb58a1a37b2bfab0461df760
NDH <ndhrones@gmail.com>
2024-03-29 14:43:45 -0400

    Update objBuilder.js

--- js/data/objBuilder.js
+++ js/data/objBuilder.js
@@ -10,14 +10,14 @@ export async function buildTestDataSet (size) {
    return new Promise((resolve, _reject) => {
       // neasure the construction
       const loadStart = performance.now()
-      const donnerMap = new Map(usersJson)
+      const tempMap = new Map(usersJson)
       
-      console.log(`time to Load ${donnerMap.size} json records - ${(performance.now() - loadStart).toFixed(2)} ms `);
-      console.log(donnerMap.get(0))
+      console.log(`time to Load ${tempMap.size} json records - ${(performance.now() - loadStart).toFixed(2)} ms `);
+      console.log(tempMap.get(0))
       const map = new Map()
       const showStart = performance.now();
       for (let index = 0; index < size; index++) {
-         const user = donnerMap.get(index).value
+         const user = tempMap.get(index).value
          user.id = index
          map.set(index, user)
       }
